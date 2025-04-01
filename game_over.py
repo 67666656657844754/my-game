2b7925d145cebeb5fa3c8488e5ca7770cede1a1d
fnafer 123 (Kovalchuk M.) <159624202+67666656657844754@users.noreply.github.com>
2025-04-01 11:16:16 +0300

    Update game_over.py

--- game_over.py
+++ game_over.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 
 root = tk.Tk()
-
-root.title("game over")
+root.title("Game Over")
 root['bg'] = "#fafafa"
 root.geometry("300x100+400+100")
 
-
 game_over = tk.Label(root, text="GAME OVER", fg="Red", bg="#fafafa", font=("Arial", 24))
 game_over.pack()
-okbutt = tk.Button(root, text="Ok", fg="Red", bg="#fafafa" ,font=("Aerial", 24))
+
+def close_window():
+    root.destroy()
+
+okbutt = tk.Button(root, text="Ok", fg="Red", bg="#fafafa", font=("Arial", 24), command=close_window)
 okbutt.pack()
 
 root.mainloop()
