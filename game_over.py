02fb751af138e30d566e2c3b603f8958f75ab925
fnafer 123 (Kovalchuk M.) <159624202+67666656657844754@users.noreply.github.com>
2025-04-01 11:15:07 +0300

    Update game_over.py

--- game_over.py
+++ game_over.py
@@ -6,7 +6,10 @@ root.title("game over")
 root['bg'] = "#fafafa"
 root.geometry("300x100+400+100")
 
+
 game_over = tk.Label(root, text="GAME OVER", fg="Red", bg="#fafafa", font=("Arial", 24))
 game_over.pack()
+okbutt = tk.Button(root, text="Ok", fg="Red", bg="#fafafa" ,font=("Aerial", 24))
+okbutt.pack()
 
 root.mainloop()
