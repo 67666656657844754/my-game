ebd619e9c053e0964db8a5b1d8b3f045bd0bc2e9
fnafer 123 (Kovalchuk M.) <159624202+67666656657844754@users.noreply.github.com>
2025-04-22 11:15:22 +0300

    Update game.py

--- game.py
+++ game.py
@@ -18,11 +18,11 @@ pl_x = 250
 pl_y = 400
 
 # Creating the enemy
-enemi = pg.Surface((50, 50))
-enemi.fill((255, 0, 0))  # RGB for the red color
+enemy = pg.Surface((50, 50))
+enemy.fill((255, 0, 0))  # RGB for the red color
 e_x = random.randint(0, 450)
 e_y = 0
-enemi_speed_y = 5
+enemy_speed_y = 5
 
 # Loading music
 pg.mixer.music.load('music/164b99c10472d02.mp3')
@@ -43,12 +43,12 @@ while running:
         pl_x += square_speed
 
     # Moving the enemy downward (falling)
-    e_y += enemi_speed_y
+    e_y += enemy_speed_y
 
     # Checking for collision between player and enemy
     if pg.Rect(pl_x, pl_y, 55, 55).colliderect(pg.Rect(e_x, e_y, 50, 50)):
         print("Collision!")# Outputs a message to the console
-        enemi.fill((255, 0, 0))  # Changes the enemy's color to green upon collision
+        enemy.fill((255, 0, 0))  # Changes the enemy's color to green upon collision
         
         # Executing the game_over.py script
         subprocess.run("python game_over.py")
@@ -64,7 +64,7 @@ while running:
 
     screen.fill((0, 0, 0))  # Clears the screen before drawing
     screen.blit(square, (pl_x, pl_y))
-    screen.blit(enemi, (e_x, e_y))
+    screen.blit(enemy, (e_x, e_y))
     pg.display.update()
 
     clock.tick(60)  # 60 frames per second
